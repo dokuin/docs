7a8fb5f69d26b827978e7ce877f6c7ed89dc34c9
muhafaa <muhafa99@gmail.com>
2020-03-24 10:08:37 +0700

    pull dev

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,53 +1,44 @@
-
-import React, { useState , useCallback} from 'react'
-
-
+import React, { useState, useCallback } from 'react'
 
 import { Navbar, Nav, Container, Image } from 'react-bootstrap'
-import { Link , useLocation} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import DokuinLogo from '../assets/logo dokuin.png'
 
 import { useEffect } from 'react'
 import styled from 'styled-components'
 
 const HoverText = styled.p`
-	color: white;
-	:hover {
-    border-bottom: 2px solid #59ADB5CC;
-	
-		cursor: pointer;
-	}
-`
+  color: white;
+  :hover {
+    border-bottom: 2px solid #59adb5cc;
 
+    cursor: pointer;
+  }
+`
 
 const Header = () => {
   const param = useLocation().pathname
-  const[position,setPost]=useState(0)
-  window.addEventListener('scroll',()=>{
+  const [position, setPost] = useState(0)
+  window.addEventListener('scroll', () => {
     const scrolled = window.scrollY
     setPost(scrolled)
   })
-  
-  const setBg =()=>{
-    if(position>250 || param !== '/'){
-      return { backgroundColor:'#1a2840'}
+
+  const setBg = () => {
+    if (position > 250 || param !== '/') {
+      return { backgroundColor: '#1a2840' }
     } else {
-      return { backgroundColor:'transparent'}
+      return { backgroundColor: 'transparent' }
     }
   }
-  useEffect(()=>{
-         
-  },[setBg])
-
+  useEffect(() => {}, [setBg])
 
   return (
     <Navbar
       collapseOnSelect
       expand="lg"
       className="m-0 d-flex justify-content-center"
-
       style={setBg()}
-
       sticky="top"
     >
       <Container className="m-0 d-flex justify-content-between">
@@ -61,19 +52,14 @@ const Header = () => {
 
         <div>
           <Nav className="">
-
-            <Link to="/docs/getting-started" className="router-link" >
+            <Link to="/docs/getting-started" className="router-link">
               <Nav.Link as="div" className="text-white font-weight-bold">
-                <HoverText>
-                Docs
-                </HoverText>
+                <HoverText>Docs</HoverText>
               </Nav.Link>
             </Link>
             <Link to="/docs/example" className="router-link">
               <Nav.Link as="div" className="text-white font-weight-bold">
-              <HoverText>
-                Example
-                </HoverText>
+                <HoverText>Example</HoverText>
               </Nav.Link>
             </Link>
 
