49c8d329a019e145df29f2a87c1c77e09c76ca4a
muhafaa <muhafa99@gmail.com>
2020-03-27 10:18:17 +0700

    rapihin button di usage

--- src/components/docs/getStarted/Installation.jsx
+++ src/components/docs/getStarted/Installation.jsx
@@ -1,61 +1,56 @@
-import React from 'react'
-import CodeBlock from '../../CodeBlock'
-import { Link } from 'react-router-dom'
-import { ArrowForward } from '@material-ui/icons';
-
+import React from "react";
+import CodeBlock from "../../CodeBlock";
+import { Link } from "react-router-dom";
+import { ArrowForward } from "@material-ui/icons";
 
 const Installation = () => {
   return (
-    <div>
-    <div className="my-5 ml-3 p-3 ">
-      <h3 className="mb-3">Installation</h3>
-      <h5>
-        Dokuin.js is made to be easily installed in your project and is designed
-        to be simple to installed
-      </h5>
-      <br />
+    <div className="my-5">
+      <div className="ml-3 p-3 ">
+        <h3 className="mb-3">Installation</h3>
+        <h5>
+          Dokuin.js is made to be easily installed in your project and is
+          designed to be simple to installed
+        </h5>
+        <br />
 
-      <ol>
-        <li>
-          <p>
-            Make sure you have the latest version of node and npm. you can check
-            it with :
-          </p>
-          <CodeBlock code={JSON.stringify('npm -v && node -v')} />
-        </li>
-        <li>
-          <p>Create your project and install npm locally in it :</p>
-          <CodeBlock code={JSON.stringify('npm init')} />
-        </li>
-        <li>
-          <p>Install dokuinjs with script :</p>
-          <CodeBlock code={JSON.stringify('npm install -g dokuinjs')} />
-        </li>
-      </ol>
-    </div>
-    <div className="ml-auto"
-    style={{
-      maxWidth:'10vw'
-    }} >
+        <ol>
+          <li>
+            <p>
+              Make sure you have the latest version of node and npm. you can
+              check it with :
+            </p>
+            <CodeBlock code={JSON.stringify("npm -v && node -v")} />
+          </li>
+          <li>
+            <p>Create your project and install npm locally in it :</p>
+            <CodeBlock code={JSON.stringify("npm init")} />
+          </li>
+          <li>
+            <p>Install dokuinjs with script :</p>
+            <CodeBlock code={JSON.stringify("npm install -g dokuinjs")} />
+          </li>
+        </ol>
+      </div>
+      <div
+        className="d-flex justify-content-end"
+      >
         <Link
-                style={{
-                  textDecoration: 'none',
-                  color: 'white',
-                  backgroundColor:'#2C4054',
-                  borderRadius:'10px',
-                }}
-                className="px-5 p-2"
-                to="/docs/usage"
-              >
-                Usage
-                <ArrowForward className="ml-3"/>
-              </Link>
+          style={{
+            textDecoration: "none",
+            color: "white",
+            backgroundColor: "#2C4054",
+            borderRadius: "10px"
+          }}
+          className="px-5 p-2"
+          to="/docs/usage"
+        >
+          Usage
+          <ArrowForward className="ml-3" />
+        </Link>
+      </div>
     </div>
-    </div>
+  );
+};
 
-     
-
-  )
-}
-
-export default Installation
+export default Installation;
